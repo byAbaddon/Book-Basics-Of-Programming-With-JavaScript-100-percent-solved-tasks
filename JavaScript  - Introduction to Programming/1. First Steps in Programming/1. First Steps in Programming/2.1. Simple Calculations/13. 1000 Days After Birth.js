992e867cefdceb66e608540f5fe4b2d7d5fb4cac
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2021-01-01 01:48:29 +0200

    Delete 13. 1000 Days After Birth.js

--- JavaScript  - Introduction to Programming/1. First Steps in Programming/1. First Steps in Programming/2.1. Simple Calculations/13. 1000 Days After Birth.js
+++
@@ -1,9 +0,0 @@
-function thousandDaysOnEarth([inputData]) {
-  let [year, month, day] = inputData.split('-').map(Number)
-  let myDate = new Date(day, --month, year)
-  myDate.setDate(myDate.getDate() + 1000)
-  let [days, months, years] = [myDate.getDate(), myDate.getMonth() + 1, myDate.getFullYear()]
-  days < 10 ? days = "0" + days : null
-  months < 10 ? months = "0" + months : null
-  return days + "-" + months + "-" + years
-}
