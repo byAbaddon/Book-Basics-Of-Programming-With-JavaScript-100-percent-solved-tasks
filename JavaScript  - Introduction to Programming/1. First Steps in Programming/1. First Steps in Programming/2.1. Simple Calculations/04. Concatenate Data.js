43f600f0f33f16ced1a9bcc91282240b8a6b2d88
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2021-01-01 01:47:33 +0200

    Delete 04. Concatenate Data.js

--- JavaScript  - Introduction to Programming/1. First Steps in Programming/1. First Steps in Programming/2.1. Simple Calculations/04. Concatenate Data.js
+++
@@ -1,6 +0,0 @@
-function functionName([name, lastName, age, town]) {
-  return `You are ${name} ${lastName}, a ${age}-years old person from ${town}.`
-}
-
-//console.log(functionName(['Ivan', 'Ivanov', '19', 'Sofia']))
-
